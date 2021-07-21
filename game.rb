1a3f9406cdd41a41ee3c421ee08325d52146302e
Nehemiah Amanuel <nehemiaha1@outlook.com>
2021-07-21 23:10:06 +0300

    Play the two players until a winner is found or draw

--- game.rb
+++ game.rb
@@ -13,6 +13,7 @@ class Game
         welcome
         register_players
         display_board
+        start_new
     end
 
     def welcome
@@ -41,6 +42,44 @@ class Game
         puts 
         puts @board.board
         puts
+    end
+
+    def start_new
+        while @board.filled_board.length != 9 do
+            current_player = @players[@current_player_id]
+
+            puts "#{current_player.name}, please enter a number (1-9)" +
+                " that is available to place an 'x'"
+            begin 
+                user_selection = gets.match(/[0-9]/)[0].to_i
+            rescue
+                puts "Sorry, this is an invalid answer. Please, try again"
+                retry
+            else
+                if @board.filled_board.include?(user_selection - 1) || user_selection > 9
+                    puts "Sorry, this is an invalid answer. Please, try again"
+                    next
+                end
+                current_player.add_move(user_selection)
+                @board.add_move(current_player.letter, (user_selection - 1))
+
+            end
+
+            display_board
+
+            if @board.won?(current_player.moves)
+                puts "GAME OVER! #{current_player.name} is the winner!"
+                break
+            end
+
+            switch_players
+        end
+
+        puts "It's a draw" if @board.filled_board.length == 9
+    end
+
+    def switch_players
+        @current_player_id == 0 ? @current_player_id = 1 : @current_player_id = 0
     end
 end
 
