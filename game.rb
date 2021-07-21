0e49a3d4ca760caf9ca1629ec5b7fcbf7a783282
Nehemiah Amanuel <nehemiaha1@outlook.com>
2021-07-21 23:01:41 +0300

    Register two players for the game

--- game.rb
+++ game.rb
@@ -11,10 +11,30 @@ class Game
 
     def play
         welcome
+        register_players
     end
 
     def welcome
         puts "Let's play a simple Tic-Tac-Toe game in the console"
         puts
     end
+
+    def register_players
+        0.upto(1) do |i|
+            puts "What is the name of player ##{i + 1}"
+            name = gets.chomp
+            puts "What 1 letter (or special character) would you like to be your game marker?"
+            puts "it can not be '#{@players[0].letter}'" if i == 1 
+            letter = gets.chomp
+
+            save_player(name, letter)
+        end
+    end
+
+    def save_player(name, letter)
+        p = Player.new(name, letter)
+        @players.push(p)
+    end
 end
+
+Game.new.play
